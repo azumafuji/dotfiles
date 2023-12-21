83e681a383b61d64a64a4e5a600ab75578a57d0e
Dean Sellis <dean@localhost.localdomain>
2023-12-21 21:33:13 +0000

    Cleanup of old files

--- profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user {:plugins [[lein-localrepo "RELEASE"]
-                  [lein-pprint "1.1.1"]
-                  [lein-ancient "RELEASE"]
-                  [lein-tar "RELEASE"]
-                  [lein-cljsbuild "RELEASE"]
-                  [lein-midje "3.1.1"]
-                  [cider/cider-nrepl "0.7.0"]]
-        :dependencies [[alembic "RELEASE"]
-                       [slamhound "RELEASE"]]}}
